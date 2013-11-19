29acb081031c51aad647c4302d2ac20f5e60c368
Josmel Yupanqui <yyupanqui@innovationssystems.com>
2013-11-19 18:03:04 -0500

    cambios

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -15,108 +16,96 @@ use Application\Form\Registrousuario;
 use Application\Form\Fieldset;
 use Mongo;
 use PhlyMongo\Mongodb;
-use Application\Model\Usuario; 
+use Application\Model\Usuario;
 use PhlyMongo\MongoConnectionFactory;
 use PhlyMongo\MongoCollectionFactory;
 use Application\Model\UsuarioCollection;
 
-class IndexController extends AbstractActionController
-{
-    
+class IndexController extends AbstractActionController {
+
     protected $usuarioMongodb;
-    public function __construct()
-	{ 
-		$this->_options = new \Zend\Config\Config ( include APPLICATION_PATH . '/config/autoload/global.php' );
-        }
-    
-        
-        public function getUsuariosMongoDb() 
-        {
+
+    public function __construct() {
+        $this->_options = new \Zend\Config\Config(include APPLICATION_PATH . '/config/autoload/global.php');
+    }
+
+    public function getUsuariosMongoDb() {
         if (!$this->usuarioMongodb) {
             $sm = $this->getServiceLocator();
-            $this->usuarioMongodb = $sm->get('Application\Model\UsuarioCollection');}
+            $this->usuarioMongodb = $sm->get('Application\Model\UsuarioCollection');
+        }
         return $this->usuarioMongodb;
-        }  
-        
-      public function indexAction()
-    {
-       $resultados=$this->getUsuariosMongoDb()->findAll();
-       $cantidad=count($resultados);
-       //echo json_encode($resultados);exit;
-       return new ViewModel(array('valores'=>$resultados,'cantidad'=>$cantidad));
     }
-     
-      public function eliminarusuarioAction()
-    {$id = $this->params()->fromQuery('id');
-     $this->getUsuariosMongoDb()->eliminarUsuario($id);
-    return $this->redirect()->toUrl($this->getRequest()->getBaseUrl().'/application/index/index');
-    } 
-      public function agregarusuarioAction()
-    {
-      $form = new Registrousuario("form");
+
+    public function indexAction() {
+        $resultados = $this->getUsuariosMongoDb()->findAll();
+
+        $cantidad = count($resultados);
+        //echo json_encode($resultados);exit;
+        return new ViewModel(array('valores' => $resultados, 'cantidad' => $cantidad));
+    }
+
+    public function eliminarusuarioAction() {
+        $id = $this->params()->fromQuery('id');
+        $this->getUsuariosMongoDb()->eliminarUsuario($id);
+        return $this->redirect()->toUrl($this->getRequest()->getBaseUrl() . '/application/index/index');
+    }
+
+    public function agregarusuarioAction() {
+        $form = new Registrousuario("form");
         $request = $this->getRequest();
-          if ($request->isPost()) {
-           $usuarios = new Usuario();
-         $form->setInputFilter($usuarios->getInputFilter());
-             $form->setData($request->getPost()); 
-                if ($form->isValid()) { 
-                   $usuarios->exchangeArray($form->getData());
-                 $this->getUsuariosMongoDb()->agregarUsuario($usuarios);
-                 return $this->redirect()->toUrl($this->getRequest()->getBaseUrl().'/application/index/index'); 
-                 }
-       }
-     return new ViewModel(array('form' => $form));
-    } 
-    
-     public function editarusuarioAction()
-    { 
-       $id = $this->params()->fromRoute('id', 0);
-
-     
-         if (!$id) {
-            return $this->redirect()->toUrl($this->getRequest()->getBaseUrl().'/application/index/index');
-         }
-         try {
-            $usuario= $this->getUsuariosMongoDb()->obtenerUsuario($id);
-         }
-         catch (\Exception $ex) {
-            return $this->redirect()->toUrl($this->getRequest()->getBaseUrl().'/application/index/index');
-         }  
-         $form = new Registrousuario();
-         $form->get('login')->setValue($usuario['login']);
-         $form->get('pass')->setValue($usuario['pass']);
-         $form->get('_id')->setValue($usuario['_id']);
-         $form->get('rol')->setValue($usuario['rol']);
-        // $form->bind($usuario);
-      $request = $this->getRequest();
-          if ($request->isPost()) {     
-           $usuarios = new Usuario();
-           $form->setInputFilter($usuarios->getInputFilter());
-             $form->setData($request->getPost()); 
-                if ($form->isValid()) {   
-                   $usuarios->exchangeArray($form->getData());
-                 $this->getUsuariosMongoDb()->agregarUsuario($usuarios,$id);
-                 return $this->redirect()->toUrl($this->getRequest()->getBaseUrl().'/application/index/index'); 
-                 }
-       }
-     return new ViewModel(array('form' => $form,'id'=>$id));
-    } 
-    
-    public function mongoConect()
-    {
-        $imagen=$this->_options->mongo->server;
-        $imagen2=$this->_options->mongo->db;
-	$m = new Mongo($imagen);
-	$db = $m->$imagen2;
-               return $db;
+        if ($request->isPost()) {
+            $usuarios = new Usuario();
+            $form->setInputFilter($usuarios->getInputFilter());
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $usuarios->exchangeArray($form->getData());
+                $this->getUsuariosMongoDb()->agregarUsuario($usuarios);
+                return $this->redirect()->toUrl($this->getRequest()->getBaseUrl() . '/application/index/index');
+            }
+        }
+        return new ViewModel(array('form' => $form));
     }
-   
-    
-   
-}   
 
-       
-    
-    
+    public function editarusuarioAction() {
+        $id = $this->params()->fromRoute('id', 0);
 
+
+        if (!$id) {
+            return $this->redirect()->toUrl($this->getRequest()->getBaseUrl() . '/application/index/index');
+        }
+        try {
+            $usuario = $this->getUsuariosMongoDb()->obtenerUsuario($id);
+        } catch (\Exception $ex) {
+            return $this->redirect()->toUrl($this->getRequest()->getBaseUrl() . '/application/index/index');
+        }
+        $form = new Registrousuario();
+        $form->get('login')->setValue($usuario['login']);
+        $form->get('pass')->setValue($usuario['pass']);
+        $form->get('_id')->setValue($usuario['_id']);
+        $form->get('rol')->setValue($usuario['rol']);
+        // $form->bind($usuario);
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $usuarios = new Usuario();
+            $form->setInputFilter($usuarios->getInputFilter());
+            $form->setData($request->getPost());
+            if ($form->isValid()) {
+                $usuarios->exchangeArray($form->getData());
+                $this->getUsuariosMongoDb()->agregarUsuario($usuarios, $id);
+                return $this->redirect()->toUrl($this->getRequest()->getBaseUrl() . '/application/index/index');
+            }
+        }
+        return new ViewModel(array('form' => $form, 'id' => $id));
+    }
+
+    public function mongoConect() {
+        $imagen = $this->_options->mongo->server;
+        $imagen2 = $this->_options->mongo->db;
+        $m = new Mongo($imagen);
+        $db = $m->$imagen2;
+        return $db;
+    }
+
+}
 
--- module/Application/src/Application/Model/UsuarioCollection.php
+++ module/Application/src/Application/Model/UsuarioCollection.php
@@ -1,86 +1,72 @@
 <?php
+
 namespace Application\Model;
+
 use PhlyMongo\MongoCollectionFactory;
 use PhlyMongo\MongoConnectionFactory;
 use MongoCollection;
 use Mongo;
-
 use Application\Model\Usuario;
 use PhlyMongo\HydratingMongoCursor;
 use Zend\Stdlib\Hydrator\ObjectProperty;
 
 class UsuarioCollection {// extends MongoCollection 
+
     protected $collection;
-    public function __construct( MongoCollection $adapter) {//Mongo $adapter
-        $this->collection=$adapter;
+
+    public function __construct(MongoCollection $adapter) {//Mongo $adapter
+        $this->collection = $adapter;
 //        return parent::__construct($adapter,'usuario');
     }
-    public function findAll(){
 
-   // $usarios=$this->collection->db_gps2->usuario->find();
+    public function findAll() {
 
-        $usarios=$this->collection->find();//$this->collection->db_gps2->usuario->find();
-
-//        $resultset = new HydratingMongoCursor(
-//        $usarios,
-//        new ObjectProperty,
-//        new Usuario() );
-//      
-       $resultados= array();
+        $usarios = $this->collection->find(); //$this->collection->db_gps2->usuario->find();
+        $resultados = array();
         foreach ($usarios as $result) {
-         $resultados[]=$result;
+            $resultados[] = $result;
         }
-        
-//        exit;
-//       // o esto:
-//        foreach ($resultset as $status) {
-//            printf('%s <%s>: %s', $status->id, $status->login, $status->rol);
-//        }
-//        exit;
-        
         return $resultados;
     }
 
+    public function obtenerUsuario($id) {
 
-//
-     public function obtenerUsuario($id)
-       {        
-        
-         $usarios=$this->collection->findOne(array('_id'=> new \MongoId($id)));
+        $usarios = $this->collection->findOne(array('_id' => new \MongoId($id)));
 
         if (!$usarios) {
-                   throw new \Exception("Could not find row $id");
-               }
-               return $usarios;
-      
-  }
-
-
-
-    public function eliminarUsuario($valor)
-       {        
-        $usarios=$this->collection->remove(array('_id'=> new \MongoId($valor)));
-        if($usarios==true)
-        {return $usarios; }
-        else{echo 'error al eliminar';}
+            throw new \Exception("Could not find row $id");
+        }
+        return $usarios;
     }
-   public function agregarUsuario(Usuario $valor,$id=null)
-   {  
-      
-      $cantidad = array('login'=>$valor->login,
-          'pass'=>$valor->pass,
-          'rol'=>$valor->rol,
-         // 'id'=>"$val"
-          );
- 
-      if($id)
-        {$usarios=$this->collection->insert($cantidad);
-        if($usarios==true)
-        {return $usarios; }}
-        else
-        { 
-         $usarios=$this->collection->update(array('_id'=> new \MongoId($id)),$cantidad);
-        if($usarios==true)
-        {return $usarios; }}
-     }
+
+    public function eliminarUsuario($valor) {
+        $usarios = $this->collection->remove(array('_id' => new \MongoId($valor)));
+        if ($usarios == true) {
+            return $usarios;
+        } else {
+            echo 'error al eliminar';
+        }
+    }
+
+    public function agregarUsuario(Usuario $valor, $id = null) {
+
+        $cantidad = array('login' => $valor->login,
+            'pass' => $valor->pass,
+            'rol' => $valor->rol,
+                // 'id'=>"$val"
+        );
+
+        if ($id) {
+            $usarios = $this->collection->insert($cantidad);
+            if ($usarios == true) {
+                return $usarios;
+            }
+        } else {
+            $usarios = $this->collection->update(array('_id' => new \MongoId($id)), $cantidad);
+            if ($usarios == true) {
+                return $usarios;
+            }
+        }
+    }
+
 }
